8dacbd84f48f702d4ec29541766484db1f74be59
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-11-17 12:42:22 -0800

    Create a deck file

--- deck.go
+++ deck.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 // Create a new type of 'deck', which is a slice of strings
 // next line means : deck ========= []string
-type deck []string
+type deck []string // deck is a type of slice of string(or array of strings)
+// Any variable of type 'deck' now gets access to the 'print' method
+func (d deck) print(){
+	for index,card:=range d{
+		fmt.Println(index,card)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
-
-import "fmt"
-
 func main() {
 	cards := deck{newCards(), "Three of Diamonds"}
 	cards = append(cards, "Ace of Soldiers")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	fmt.Println((cards))
+	makan := deck{"fname:Makan","lname:Fard"}
+	cards.print()
+	makan.print()
 }
 func newCards() string {
 	return "Ace of Spades"
