b341c3a897095793bc3b9bbbc620dfaab1b3a03a
Fojan Babaali <fojan.Babaali@carmanah.com>
2024-01-05 14:15:52 -0800

    recap

---
+++ cards/deck.go
@@ -0,0 +1,5 @@
+package main
+
+//Create a type of 'deck'
+//which is a slice of string
+type deck []string
--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 func main(){
 	// How to declare a new slice?
-	cards := [] string{"Ace of Spades",newCard()}
+	cards := deck{"Ace of Spades",newCard()}
 	// How to add new data to an existing slice?
 	cards = append(cards,"Six of Diamonds")
 	// How to loop through a slice in GO?
