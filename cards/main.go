f4ebdc65697e0e3da769f9aa0061b2ba6a74cedc
fojanb <fozhanmail@gmail.com>
2024-01-06 18:26:43 -0800

    Error Handling

--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,12 @@
-// Multiple Returns Value
+// Type Conversion in GO - We need this to use WriteFile function inside our application
 package main
+import "fmt"
 func main(){
-	cards := newDeck()
-	//cards.print()
-	hand,remains := cards.deal(4)
-	hand.print()
-	remains.print()
-
+	// var greeting string= "Hello"
+	// fmt.Println([]byte(greeting))
+	//cards := newDeck()
+	//fmt.Println(cards.toString())
+	//fmt.Println([]byte(cards.toString()))
+	//cards.saveToFile()
+	fmt.Println(readFromFile("test"))
 }
